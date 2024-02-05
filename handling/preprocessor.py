5f5de85bfe4ac076192426f7b492e8cd9a786543
Peter Bitman <peter.bitman@gmail.com>
2024-02-05 17:31:26 +0000

    initial working preprocessor - final rough draft

--- handling/preprocessor.py
+++ handling/preprocessor.py
@@ -8,21 +8,16 @@ from datetime import datetime
 def preprocess_data(size=('full', 'noon')):
     """
     Preprocesses images from file. Returns metadata in a dataframe, and a np array of image data.
-
-    Takes keywords ['full': entire dataset, 'noon': all images at 12pm]
+    Use
     """
 
-    def process_image(image):
-
-
-    folder_path = "raw_data/PanelImages"
-    image_data = []  # List to store image data
+    folder_path = "../raw_data/PanelImages"
+    image_data = [] # initialise an empty array to stack the images
     metadata = []
     # Regular expression pattern to extract date and intensity values from the filename
 
     # capped at 1000 for now
     for filename in os.listdir(folder_path):
-
         split_name = filename.split('_')
         hour = split_name[4]
         if size == 'noon' and hour != '12':
@@ -39,6 +34,7 @@ def preprocess_data(size=('full', 'noon')):
 
         # append metadata to list
         filename_info = [month, weekday, day, hour, minute, second, year, datetime_obj, age_loss, irradiance_level]
+
         metadata.append(filename_info)
 
         file_path = os.path.join(folder_path, filename)
@@ -46,20 +42,25 @@ def preprocess_data(size=('full', 'noon')):
         # Load the image using OpenCV
         image = cv2.imread(file_path)
 
+        # Resize the image to 224x224 using bilinear interpolation - OPTION to save the resized images so this never is done again!
+        resized_image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_LINEAR)
+
         # Convert the image to a numpy array (tensor)
         # OpenCV loads images in BGR format by default, this convert to RGB
-        image_array = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image_array.append(image_array)
+        image_array = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
+        image_data.append(image_array)
 
-        filename_info.append(image_array)
-        image_data.append(filename_info)
-
-
-        # Convert the list of tuples to a pandas DataFrame
-    df = pd.DataFrame(image_data, columns=['Month', 'Day', 'Date', 'Hour', 'Minute', 'Second', 'Year',
-                                        'Datetime', 'Percent Age Loss', 'Irradiance Level'])
+    # Convert the list of tuples to a pandas DataFrame
+    df = pd.DataFrame(metadata, columns=['Month', 'Day', 'Date', 'Hour', 'Minute', 'Second', 'Year',
+                                        'Datetime', 'Percentage Loss', 'Irradiance Level'])
 
     df = df.astype({'Month': str, 'Day': str, 'Date': int, 'Hour': int, 'Minute': int, 'Second': int, 'Year': int,
                                        'Datetime': 'datetime64[ns]', 'Percentage Loss': float, 'Irradiance Level': float})
 
+    # convert image data to numpy arrays
+    image_data = np.array(image_data)
+    # normalize the image data
+    image_data = image_data / 255.0
+
+    # return metadata and normalized image data
     return df, image_data
