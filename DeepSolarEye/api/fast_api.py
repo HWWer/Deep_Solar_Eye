5fe70956d88d4fd196ef08c1e706daf317dca457
Alexander Simon <alexander.t.simon@gmail.com>
2024-02-09 18:38:15 +0100

    Latest Api predict loss function

--- DeepSolarEye/api/fast_api.py
+++ DeepSolarEye/api/fast_api.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI, UploadFile, File
 from PIL import Image
 from fastapi.middleware.cors import CORSMiddleware
+import sys
+sys.path.append('..')
 from DeepSolarEye.dl_logic.predict import predict_loss
 from DeepSolarEye.dl_logic.model import regression_ResNet
+
 app = FastAPI()
 
-app.state.model= regression_ResNet()
+
 
 
 
@@ -20,10 +23,11 @@ def root():
 @app.post("/predict")
 async def receive_image(img: UploadFile=File(...)):
    ### Receiving and decoding the image
-
+    filename=img.filename
     img = await img.read()
+    model=regression_ResNet(model_name='ResNet50', input_shape=(224, 224, 3),input_time_irradiance=(2,), num_units=512, pretrained=True)
 
-    model=app.state.model()
 
-    prediction = predict_loss(model,img)
-    return {'power_loss':prediction}
+    prediction = predict_loss(model,img,filename)
+    return {'power_loss':prediction,
+            'filename': filename}
--- DeepSolarEye/handling/preprocessor.py
+++ DeepSolarEye/handling/preprocessor.py
@@ -106,8 +106,11 @@ def time_encoder(df: pd.DataFrame, hour_col, minute_col, second_col):
 def preprocess_img(img):
     '''Preprocess the image from user and transforms it into a Dataset for model input'''
     img = tf.image.decode_jpeg(img, channels=3)
+    #img = tf.convert_to_tensor(img, dtype=tf.uint8)
+
     img = tf.image.resize(img, [224, 224])
     img = preprocess_input(img)
-    img = tf.data.Dataset(img)
-    images_ds = tf.data.Dataset.from_tensor_slices(img)
+
+    images_ds = tf.data.Dataset.from_tensors(img)
+
     return images_ds
